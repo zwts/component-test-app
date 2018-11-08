2797bed977509b50451f7dec448a26c437ae1ae4
Tan Shi <shi.tan@kaiostech.com>
2018-11-08 18:15:40 +0800

    Add panes; pane will change with tab change; a gaiaheader like header

--- js/app.js
+++ js/app.js
@@ -1,24 +1,35 @@
 'use strict';
 
 window.onload = () => {
-  //....
 
+  // handle the tab change event, then give the reflect of tab pane
+  (function() {
+    let tabs = document.querySelector('gaia-tabs');
+    let panes = document.querySelector('.panes');
+    tabs.addEventListener('change', () => {
+      let selectedIndex = tabs.getAttribute('selected');
+      panes.querySelector('.selected').classList.remove('selected');
+      panes.querySelectorAll('.pane')[selectedIndex].classList.add('selected');
+    });
+  })();
+
+  // handle left and right key change the tab nav index
   window.addEventListener('keydown', (evt) => {
-    let tab = document.getElementById('tablist');
-    let selectedIndex = tab.getAttribute('selected');
+    let tabs = document.querySelector('gaia-tabs');
+    let selectedIndex = tabs.getAttribute('selected');
     let tablen = 4;
     let nextIndex;
     switch (evt.key) {
       case 'ArrowRight':
         nextIndex = parseInt(selectedIndex) + 1;
         if (nextIndex <= tablen - 1) {
-          tab.setAttribute('selected', nextIndex);
+          tabs.setAttribute('selected', nextIndex);
         }
         break;
       case 'ArrowLeft':
         nextIndex = parseInt(selectedIndex) - 1;
         if (nextIndex >= 0) {
-          tab.setAttribute('selected', nextIndex);
+          tabs.setAttribute('selected', nextIndex);
         }
         break;
     }
